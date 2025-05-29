617d48c070329530b5571d0e0b7cec7074a1e53e
Jose Marcel <96666524+LopezJoseMarcel@users.noreply.github.com>
2025-05-28 21:48:52 -0300

    Upadate Final

--- src/controllers/scraping.controller.ts
+++ src/controllers/scraping.controller.ts
@@ -6,7 +6,7 @@ export const startFullScraping = async (req: Request, res: Response) => {
   const { endPage, tranType, propType, city, departamento} = req.body;
 
   scrapeAllPagesAndSave(endPage,tranType, propType, city,departamento)
-    .then(() => console.log("Scraping corriendo en el backend"))
+    .then(() => console.log("Scraping Terminado"))
     .catch((err) => console.error("Error scraping:", err));
 
   res.status(202).json({ message: "Scraping iniciado. Scraping corriendo en el backend." });
